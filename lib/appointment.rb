c339c6ee9ba18c913bbe4cf09a2eaf5ef380fa18
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 20:43:42 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,14 +1,14 @@
-class Appointment 
+class Appointment
    attr_accessor :date, :patient, :doctor
    @@all = []
    def initialize(date, patient, doctor)
        @date = date
-       @patient = patient 
+       @patient = patient
        @doctor = doctor
        @@all << self
    end
-   def self.all 
+   def self.all
        @@all
    end
-  
+
 end
