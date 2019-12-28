dc9aae8bfb7a8a43561a9d3585ed0a482ba8f4d0
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 20:42:40 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,35 +1,21 @@
 require 'pry'
-
-class Doctor
-
-  attr_reader :name, :appointment, :patient
-
-  @@all_doc = []
-
-  def initialize(name)
-    @name = name
-    @@all_doc << self
-  end
-
-  def self.all
-    @@all_doc
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
-  end
-
-  def appointments
-    Appointment.all.select do |appointment|
-    appointment.doctor == self
-    binding.pry
+class Doctor 
+    attr_reader :name
+    @@all = [] 
+    def initialize(name)
+        @name = name
+        @@all << self 
     end
-  end
-
-  def patients
-    appointments.map do |appointment|
-      appointment.patient
+    def self.all
+        @@all
     end
-  end
-
+    def appointments
+        Appointment.all.select {|a| a.doctor == self}
+    end
+    def new_appointment(patient, date)
+        Appointment.new(date, patient,self)
+    end
+    def patients 
+        appointments.map {|a| a.patient}
+    end
 end
