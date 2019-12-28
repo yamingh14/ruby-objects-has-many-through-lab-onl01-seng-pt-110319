149f634f5a2a57c5a6ee83f0d8b9e15ca9408403
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 20:44:30 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Doctor
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def appointments
+        Appointment.all.select {|a| a.doctor == self}
+    end
+    def new_appointment(patient, date)
+        Appointment.new(date, patient,self)
+    end
+    def patients
+        appointments.map {|a| a.patient}
+    end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Patient
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def appointments
+        Appointment.all.select {|a| a.patient == self}
+    end
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+    def doctors
+        appointments.map {|a| a.doctor}
+    end
+end
