76e345823e8a6f1a85140702585f9c622d2cdda1
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 19:59:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  ALL = []
+
+  def initialize(name="", artist="", genre="")
+    @name = name
+    @artist = artist
+    @genre = genre
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def artist
+    @artist
+  end
+
+end
